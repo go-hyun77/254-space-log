981567bf920fca6ba28ddc500526878c3eefaea7
mrdistractions <31258395+mrdistractions@users.noreply.github.com>
2018-04-04 06:18:49 -0700

    Add PLANET LOG
    UPDATES:
    -Add "import planet"
    -Enable the -p function
    
    Added Filed: "Planet.py"
    -Searches for key word: "BodyName" : "<planetname>"
    -Prints out full planet names
    -Return total planets scanned.

---
+++ planet.py
@@ -0,0 +1,20 @@
+
+import re
+
+def get_planet_list(content:str):
+	pattern = re.compile("\"BodyName\":\"[-\w+\s\d+]+")
+	planets = pattern.findall(content)	
+	if planets:
+		for r in planets:
+
+			if r[0] == r[-1]:
+				r +=1
+			print (r+'"')
+	return 	len(planets)	
+	
+
+
+
+
+
+
